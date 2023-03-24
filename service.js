388f3302c1414239171f6106d83f8cc6e0b916dc
Nathan <65243509+NathanHGit@users.noreply.github.com>
2023-03-24 11:02:22 +0100

    training subject - final

--- service.js
+++ service.js
@@ -1,39 +1,37 @@
-const fs = require("fs");
-let data = require("./data.json");
+const fs = require('fs');
+let data = require('./data.json');
 
 function saveData() {
-  fs.writeFile("data.json", JSON.stringify(data), (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("JSON data is saved.");
-  });
+    fs.writeFile('data.json', JSON.stringify(data), (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log('JSON data is saved.');
+    });
 }
 
 module.exports = function (app) {
-  app.get("/history", function (request, response) {
-    response.send(data.history);
-  });
+    app.get('/connection', function (request, response) {
+        response.send('Back connection done!');
+    });
 
-  app.post("/history/add", function (request, response) {
-    data.history.unshift(request.body);
-    saveData();
-    response.send(true);
-  });
+    app.get('/history', function (request, response) {
+        response.send(data.history);
+    });
 
-  app.get("/bookmarks", function (request, response) {
-    response.send(data.bookmarks);
-  });
+    app.post('/history/save', function (request, response) {
+        data.history = JSON.parse(request.body);
+        saveData();
+        response.send(true);
+    });
 
-  app.post("/bookmarks/add", function (request, response) {
-    data.bookmarks.unshift(request.body);
-    saveData();
-    response.send(true);
-  });
+    app.get('/bookmarks', function (request, response) {
+        response.send(data.bookmarks);
+    });
 
-  app.post("/bookmarks/remove", function (request, response) {
-    data.bookmarks = data.bookmarks.filter((v) => v !== request.body);
-    saveData();
-    response.send(true);
-  });
+    app.post('/bookmarks/save', function (request, response) {
+        data.bookmarks = JSON.parse(request.body);
+        saveData();
+        response.send(true);
+    });
 };
